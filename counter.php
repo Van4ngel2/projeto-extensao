20930132af5c9fb7445a9f357de408aeb23e105f
Van4ngel <110430887+Van4ngel2@users.noreply.github.com>
2024-06-23 19:16:17 -0300

    Create counter.php

---
+++ counter.php
@@ -0,0 +1,21 @@
+<?php
+// Arquivo que armazenará o contador de acessos
+$counterFile = 'counter.txt';
+
+// Verifica se o arquivo existe, se não, cria com valor inicial 0
+if (!file_exists($counterFile)) {
+    file_put_contents($counterFile, '0');
+}
+
+// Lê o valor atual do contador
+$counter = (int)file_get_contents($counterFile);
+
+// Incrementa o contador
+$counter++;
+
+// Salva o novo valor no arquivo
+file_put_contents($counterFile, (string)$counter);
+
+// Retorna o valor do contador como resposta
+echo $counter;
+?>
